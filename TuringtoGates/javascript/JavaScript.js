b31acb56c5484ecfdee382469750ceab6c738dad
Moayad Myro <moayadmyro@gmail.com>
2022-04-18 06:39:38 -0700

    TuringtoGates

---
+++ TuringtoGates/javascript/JavaScript.js
@@ -0,0 +1,13 @@
+﻿jQuery(document).ready(function () {
+    $('img').click(function () {
+        console.log('alt-pic is', $(this).attr('alt-pic'));
+        let ss = $(this).attr('alt-pic');
+        if (ss[1] === 'a') {
+            ss=  ss.replace('a','b');
+            console.log(ss);
+        }
+        else { ss = ss.replace('b', 'a'); console.log(ss); }
+        $(this).attr('src','img/'+ ss);
+    });
+   
+});
